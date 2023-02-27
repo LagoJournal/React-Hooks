66f485e52444d59458e169211a22567b2851866c
Agustin Lago <agustin.lago@mi.unc.edu.ar>
2023-02-27 20:11:18 -0300

    Essential Hooks, useState

---
+++ react-hooks-explained/src/App.js
@@ -0,0 +1,12 @@
+import { UseState, UseEffect } from "./Pages/essentials/index";
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+      <UseEffect />
+    </div>
+  );
+}
+
+export default App;
